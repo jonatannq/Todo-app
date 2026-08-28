1ceed056c649903019d0df1ab074842e9426bdc7
jonatan <jonatanaguilarmendez@gmail.com>
2026-08-28 13:33:53 -0600

    Agregar tareas a lista

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,46 @@
-
+import { useState } from "react"
+import Tarea from './component/tarea.jsx'
 
 function App() {
+
+  const [tarea, setTarea] = useState("")
+  const [tareas, setTareas] = useState([])
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    if(!tarea){
+        alert("Tarea vacia")
+        return
+    }
+
+    setTareas([...tareas, tarea])
+    setTarea("")
+  } 
 
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>Todo App List</h1>
+
+      {
+        tareas.map((tarea) => {
+          return(
+            <Tarea name={tarea}/>
+          )
+        })
+      }
+      
+      <form onSubmit={handleAdd}>
+        <input type="text" value={tarea} placeholder="Escribe tu tarea" onChange={(e) => setTarea(e.target.value)} />
+        <button>YES</button>
+      </form>
+
     </>
   )
+  
+  
+
+
 }
 
 export default App
---
+++ FrontEnd/src/component/tarea.jsx
@@ -0,0 +1,11 @@
+
+
+function Tarea({name}){
+    return(
+    <>
+        <h1>{name}</h1>
+    </>
+    )
+}
+
+export default Tarea
